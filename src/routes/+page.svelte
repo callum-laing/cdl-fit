6fb43f067a013fcbd04ee66b0040f5f7caf3698f
Callum Laing <callum.laing91@hotmail.co.uk>
2024-06-21 21:16:26 +0100

    added initial setup

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,20 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<h1>cdl-fit</h1>
+<p>Happily track your workouts and nutrition :)</p>
+
+<style>
+	h1 {
+		color: blue;
+		text-align: center;
+		font-size: 4rem;
+	}
+
+	p {
+		text-align: center;
+		font-size: 2em;
+	}
+
+	h1,
+	p {
+		font-family: Georgia, 'Times New Roman', Times, serif;
+	}
+</style>
